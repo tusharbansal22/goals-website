61a29d07874159d20ee767342c6de455125340e7
tusharb <tusharbansal@iitbhilai.ac.in>
2023-06-19 20:57:07 +0530

    uploaded images

--- js/script2.js
+++ js/script2.js
@@ -1,26 +1,86 @@
-const textm1= document.querySelector('.img-box-text .m1')
+const textc1= document.querySelector('.img-box-text #c1')
+textc1.innerHTML=textc1.innerText.split("").map(
+  (char,i)=>
+  `<span style="transform:rotate(${20*i}deg)">${char}</span>`
+).join("")
+const textc2= document.querySelector('.img-box-text #c2')
+textc2.innerHTML=textc2.innerText.split("").map(
+  (char,i)=>
+  `<span style="transform:rotate(${20*i}deg)">${char}</span>`
+).join("")
+const textm1= document.querySelector('.img-box-text #m1')
 textm1.innerHTML=textm1.innerText.split("").map(
   (char,i)=>
   `<span style="transform:rotate(${20*i}deg)">${char}</span>`
 ).join("")
-const textm2= document.querySelector('.img-box-text .m2')
+const textm2= document.querySelector('.img-box-text #m2')
 textm2.innerHTML=textm2.innerText.split("").map(
   (char,i)=>
-  `<span style="transform:rotate(${11*i}deg)">${char}</span>`
+  `<span style="transform:rotate(${20*i}deg)">${char}</span>`
 ).join("")
-const textc1= document.querySelector('.img-box-text .c1')
-textc1.innerHTML=textc1.innerText.split("").map(
+const textm3= document.querySelector('.img-box-text #m3')
+textm3.innerHTML=textm3.innerText.split("").map(
   (char,i)=>
-  `<span style="transform:rotate(${11*i}deg)">${char}</span>`
+  `<span style="transform:rotate(${20*i}deg)">${char}</span>`
 ).join("")
-const textc2= document.querySelector('.img-box-text .c2')
-textc2.innerHTML=textc2.innerText.split("").map(
+const textm4= document.querySelector('.img-box-text #m4')
+textm4.innerHTML=textm4.innerText.split("").map(
   (char,i)=>
-  `<span style="transform:rotate(${11*i}deg)">${char}</span>`
+  `<span style="transform:rotate(${8*i}deg)">${char}</span>`
 ).join("")
-const textc3= document.querySelector('.img-box-text .c3')
-textc3.innerHTML=textc3.innerText.split("").map(
+const textm5= document.querySelector('.img-box-text #m5')
+textm5.innerHTML=textm5.innerText.split("").map(
   (char,i)=>
-  `<span style="transform:rotate(${11*i}deg)">${char}</span>`
+  `<span style="transform:rotate(${20*i}deg)">${char}</span>`
+).join("")
+const textm6= document.querySelector('.img-box-text #m6')
+textm6.innerHTML=textm6.innerText.split("").map(
+  (char,i)=>
+  `<span style="transform:rotate(${20*i}deg)">${char}</span>`
+).join("")
+const textm7= document.querySelector('.img-box-text #m7')
+textm7.innerHTML=textm7.innerText.split("").map(
+  (char,i)=>
+  `<span style="transform:rotate(${20*i}deg)">${char}</span>`
+).join("")
+const textm8= document.querySelector('.img-box-text #m8')
+textm8.innerHTML=textm8.innerText.split("").map(
+  (char,i)=>
+  `<span style="transform:rotate(${20*i}deg)">${char}</span>`
+).join("")
+const textm9= document.querySelector('.img-box-text #m9')
+textm9.innerHTML=textm9.innerText.split("").map(
+  (char,i)=>
+  `<span style="transform:rotate(${20*i}deg)">${char}</span>`
+).join("")
+const textm10= document.querySelector('.img-box-text #m10')
+textm10.innerHTML=textm10.innerText.split("").map(
+  (char,i)=>
+  `<span style="transform:rotate(${20*i}deg)">${char}</span>`
+).join("")
+const textm11= document.querySelector('.img-box-text #m11')
+textm11.innerHTML=textm11.innerText.split("").map(
+  (char,i)=>
+  `<span style="transform:rotate(${12*i}deg)">${char}</span>`
+).join("")
+const textm12= document.querySelector('.img-box-text #m12')
+textm12.innerHTML=textm12.innerText.split("").map(
+  (char,i)=>
+  `<span style="transform:rotate(${20*i}deg)">${char}</span>`
+).join("")
+const textm13= document.querySelector('.img-box-text #m13')
+textm13.innerHTML=textm13.innerText.split("").map(
+  (char,i)=>
+  `<span style="transform:rotate(${20*i}deg)">${char}</span>`
+).join("")
+const textm14= document.querySelector('.img-box-text #m14')
+textm14.innerHTML=textm14.innerText.split("").map(
+  (char,i)=>
+  `<span style="transform:rotate(${20*i}deg)">${char}</span>`
+).join("")
+const textm15= document.querySelector('.img-box-text #m15')
+textm15.innerHTML=textm15.innerText.split("").map(
+  (char,i)=>
+  `<span style="transform:rotate(${20*i}deg)">${char}</span>`
 ).join("")
 
